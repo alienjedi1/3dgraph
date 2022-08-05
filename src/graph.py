9566940d8a15bf466fba9b2ee0a34c1882487f50
alien_jedi <68756367+lucasrpatten@users.noreply.github.com>
2022-08-05 17:21:40 +0000

    idk

--- src/graph.py
+++ src/graph.py
@@ -3,37 +3,43 @@ from mpl_toolkits import mplot3d
 import numpy as np
 import math
 import time
-from tkinter import *
+import tkinter as tk
 
-def set():
-    t = np.linspace(-299792458,299792459,1000)
+
+def initalize():
+    t = np.linspace(-299792458, 299792459, 1000)
 
     x = np.cos(t)
     y = np.sin(t)
     h = np.arcsinh(t)
     z = np.sin(h)
-    return([x, y, z, t])
+    return ([x, y, z, t])
+
+
 class Parametric3D:
-    def __init__(self, x, y, z, t=set()[3]):
+    def __init__(self, x, y, z, t=initalize()[3]):
         self.x = x
         self.y = y
         self.z = z
-        print(set()[2])
-        ax = plt.axes(projection = '3d')
+        print(initalize()[2])
+        ax = plt.axes(projection='3d')
 
         ax.plot3D(x, y, z, color='black', linewidth=0.03)
         plt.show()
+
 
 class GUI:
     def __init__(self, master):
         self.master = master
         master.title("GUI")
-        inputtxt = Text(Tk(), height=5, width=20)
+        inputtxt = tk.Text(root, height=5, width=20)
         inputtxt.pack()
-        daButton = Button(Tk(), text="this", command=saveVar(inputtxt))
+        daButton = tk.Button(root, text="this", command=print("hi"))
         daButton.pack()
 
-#root = Tk()
-#my_gui = GUI(root)
-#root.mainloop()
-Parametric3D(set()[0], set()[1], set()[2])
+
+Parametric3D(initalize()[0], initalize()[1], initalize()[2])
+
+# root = tk.Tk()
+# my_gui = GUI(root)
+# root.mainloop()
